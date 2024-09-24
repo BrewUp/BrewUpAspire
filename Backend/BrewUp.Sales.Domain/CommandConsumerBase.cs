336955dff3b7c259ed22c934b6ab310be136daf1
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-09-24 08:42:10 +0200

    Create a CommandConsumerBase as example

---
+++ Backend/BrewUp.Sales.Domain/CommandConsumerBase.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Messaging.ServiceBus;
+using BrewUp.Sales.Shared;
+
+namespace BrewUp.Sales.Domain;
+
+public abstract class CommandConsumerBase<T> where T : class
+{
+    protected readonly ServiceBusProcessor Processor;
+    protected readonly AzureEventBus AzureEventBus;
+
+    protected CommandConsumerBase(AzureServiceBusConfiguration azureServiceBusConfiguration,
+        AzureEventBus eventBus)
+    {
+        AzureEventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+        var serviceBusClient = new ServiceBusClient(azureServiceBusConfiguration.ConnectionString);
+        
+        Processor = serviceBusClient.CreateProcessor(
+            topicName: typeof(T).Name,
+            subscriptionName: "", new ServiceBusProcessorOptions
+            {
+                AutoCompleteMessages = false,
+                MaxConcurrentCalls = azureServiceBusConfiguration.MaxConcurrentCalls
+            });
+    }
+}
